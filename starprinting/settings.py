7e1b7e5836a25e4f8bc9836f6a459ceba688ae9a
JebastinY7 <yjebastin@gmail.com>
2026-05-02 16:47:10 +0530

    Fix Railway 400 bad request

--- starprinting/settings.py
+++ starprinting/settings.py
@@ -34,6 +34,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
