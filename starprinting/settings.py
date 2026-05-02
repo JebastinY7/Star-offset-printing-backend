17db47b8a9e63d4177232d913a621db4cc3e58a4
JebastinY7 <yjebastin@gmail.com>
2026-05-02 19:59:28 +0530

    Production security settings

--- starprinting/settings.py
+++ starprinting/settings.py
@@ -31,9 +31,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-fallback-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "admin.starprinting.in",
+    "https://web-production-d87ac.up.railway.app",
+]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
